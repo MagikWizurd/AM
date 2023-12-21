6ad6ae41da2aacddbea43a08700f80ecbe9b41f1
MagikWizurd <151967914+MagikWizurd@users.noreply.github.com>
2023-12-21 14:15:37 -0700

    Add files via upload

--- Projects/Drawing/main.js
+++ Projects/Drawing/main.js
@@ -1,42 +1,42 @@
-var color1 = ""
+var color = ""
 
 
 function changeColor(col){
-    color1 = col
+    color = col
 }
 
 window.addEventListener("load",()=> {
+    const canvas = document.querySelector("#canvas")
+    const ctx = canvas.getContext("2d")
 
-const canvas = document.querySelector("#canvas")
-const ctx = canvas.getContext("2d")
+    canvas.height = window.innerHeight
+    canvas.width=window.innerWidth
 
-canvas.height = window.innerHeight
-canvas.width=window.innerWidth
+    let painting = false
+    function startPos(e){
+        painting=true
+        draw(e)
+    }
 
-let painting = false
-function startPos(e){
-painting=true
-draw(e)
-}
+    function endPos(e){
+        painting=false
+        ctx.beginPath()
+    }
 
-function endPos(e){
-painting=false
-ctx.beginPath()
-}
+    function draw(e) {
+        if (!painting) return
 
-function draw(e) {
-    if (!painting) return
-    ctx.lineWidth=10
-    ctx.lineCap = "round"
-    ctx.strokeStyle = color1
-    ctx.lineTo(e.clientX,e.clientY)
-    ctx.stroke()
-}
+        width=document.getElementById("lineThing").value
+        if (width =="") width = 10
+        ctx.lineWidth=width
+        
+        ctx.lineCap = "round"
+        ctx.strokeStyle = color  //To change the color
+        ctx.lineTo(e.clientX,e.clientY)
+        ctx.stroke()
+    }
 
-canvas.addEventListener("mousedown",startPos)
-canvas.addEventListener("mouseup",endPos)
-canvas.addEventListener("mousemove",draw)
-
-
-
+    canvas.addEventListener("mousedown",startPos)
+    canvas.addEventListener("mouseup",endPos)
+    canvas.addEventListener("mousemove",draw)
 })
